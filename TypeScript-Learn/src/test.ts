bc148f5f19388e327ccb5b4de0099835f958a686
xzhc <1838998336@qq.com>
2025-07-16 14:41:59 +0800

    函数类型和重载

--- TypeScript-Learn/src/test.ts
+++ TypeScript-Learn/src/test.ts
@@ -116,3 +116,72 @@ set.add(2);
 const map = new Map<number, string>();
 map.set(1, "1");
 map.set(2, "2");
+
+//JS中函数表达式的表达式和函数声明的两种写法
+const handler1 = function (args) {}; //函数表达式
+const handler2 = (args) => {}; //箭头表达式
+
+function handler3(args) {} //函数声明
+
+//TS中对函数的类型约束
+
+function sum1(a: number, b: number): number {
+  return a + b;
+} //函数声明
+
+const sum2 = function (a: number, b: number): number {
+  return a + b;
+}; //函数表达式
+
+type Sum = (a: number, b: number) => number;
+const sum3: Sum = function (a, b) {
+  return a + b;
+};
+
+// JavaScript 的函数中，如果没有显式的 return 语句，那么这个函数的执行结果实际会是 undefined，
+// 但在 TypeScript 中，我们需要将这个函数的返回值类型标注为 void 而不是 undefined
+
+function handler4(): undefined {} //错误
+function handler5(): void {} // 正确
+
+//在 TypeScript 中，undefined 也被视为一个有意义的类型。
+// 因此如果你希望将返回值类型标注为 undefined，就需要有显式的 return 语句：
+function handler6(): undefined {
+  return;
+}
+
+/**
+ *在 5.1 版本中，TS 对这个不符直觉的问题进行了修正，即允许了 undefined 作为无显式 return 语句函数的返回值类型，
+ 但考虑到发布时间较晚，因此还是有必要了解这个问题的。
+ */
+
+function Sum4(x: any, y: any) {
+  if (typeof x === "number" && typeof y === "number") {
+    return x + y;
+  } else if (Array.isArray(x) && typeof y === "number") {
+    return x.map((num) => num + y);
+  } else if (typeof x === "number" && Array.isArray(y)) {
+    return y.map((num) => num + x);
+  } else if (Array.isArray(x) && Array.isArray(y)) {
+    if (x.length !== y.length) {
+      throw new Error("Arrays must have the same length");
+    }
+    return x.map((num, index) => num + y[index]);
+  } else {
+    throw new Error("Invalid arguments");
+  }
+}
+
+console.log(Sum4(1, 2));
+console.log(Sum4([1, 2, 3], 4));
+console.log(Sum4(1, [1, 2, 3]));
+console.log(Sum4([1, 2, 3], [1, 2, 3]));
+console.log(Sum4([1, 2, 3], [4, 5]));
+console.log(Sum4("a", "b"));
+
+//为了解决该问题，TS支持类型层面的重载
+function Sum5(base: number, incre: number): number;
+function Sum6(base: number, incre: number[]): number[];
+function Sum7(base: number[], incre: number): number[];
+function Sum8(base: number[], incre: number[]): number[];
+function Sum9(x: number || number[], y: number || number[]) {}
